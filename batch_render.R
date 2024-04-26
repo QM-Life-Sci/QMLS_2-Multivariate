99183cec4a16a360f1be86acf4fa3c6a13e8293f
Kevin Middleton <middletonk@missouri.edu>
2024-04-26 16:24:21 -0500

    Update batch_render.R

--- batch_render.R
+++ batch_render.R
@@ -1,9 +1,24 @@
-qmds <- list.files(pattern = "^[5].[1-5].*qmd$",
+# Lecture slides
+
+qmds <- list.files(pattern = "^[1-5].[1-5].*qmd$",
                    path = ".",
                    full.names = TRUE)
 
 for (qq in qmds) {
   message("Processing ", qq)
   syscall <- paste0("quarto render ", qq, " --to revealjs")
+  system(syscall)
+}
+
+
+## Problem sets
+
+qmds <- list.files(pattern = "^PS_[1-4][_Key]*.qmd$",
+                   path = "Problem_Sets",
+                   full.names = TRUE)
+
+for (qq in qmds) {
+  message("Processing ", qq)
+  syscall <- paste0("quarto render ", qq, " --to html")
   system(syscall)
 }
